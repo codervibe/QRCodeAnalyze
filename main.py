5ac46dae035c722cc5a6d586beaa272ed79a412a
codervibe <3507557934@qq.com>
2024-07-15 15:04:50 +0800

    增加 程序终端代码

--- main.py
+++ main.py
@@ -43,5 +43,10 @@ if __name__ == "__main__":
     # 获取用户输入的二维码图片路径
     image_path = input("请输入二维码图片的路径: ")
 
-    # 调用解码函数
-    decode_qrcode(image_path)
+    try:
+        # 调用解码函数
+        decode_qrcode(image_path)
+    except KeyboardInterrupt:
+        print("程序被中断")
+        exit()
+
